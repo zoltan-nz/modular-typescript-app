5923fa99fc625786b6f824411a592f3085aca1ed
Zoltan <zdebre@gmail.com>
2020-11-17 15:23:00 +1300

    Add more eslint config and plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,33 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
   env: {
     es2021: true,
     jest: true,
     node: true,
   },
-  plugins: ['@typescript-eslint', 'jest'],
-  extends: ['eslint:recommended', 'prettier', 'plugin:node/recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'jest', 'import'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/recommended',
+    'plugin:node/recommended',
+  ],
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.json', '.ts', '.tsx'],
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+  rules: {
+    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+  },
 };
